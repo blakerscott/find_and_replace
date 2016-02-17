53da8944dab4953bf4258e28f0e11b572a028dd1
Blake Scott and Jon Nardine <jwnardine@gmail.com>
2016-02-17 15:29:12 -0800

    pass tests for instances of a word within a word

--- tests/FindAndReplaceTest.php
+++ tests/FindAndReplaceTest.php
@@ -19,6 +19,36 @@
 		//Assert
 		$this->assertEquals('Hello mars', $result);
 		}
+
+		function test_findReplaceFunction_moreThanOneWord()
+		{
+		//Arrange
+		$test_FindReplace = new FindReplace;
+		$replaceThis = 'world';
+		$replaceWithThis = 'mars';
+		$sentenceToBeChanged = 'Hello world what such a beautiful world';
+
+		//Act
+		$result = $test_FindReplace->findReplaceFunction($replaceThis, $replaceWithThis, $sentenceToBeChanged);
+
+		//Assert
+		$this->assertEquals('Hello mars what such a beautiful mars', $result);
+		}
+
+		function test_findReplaceFunction_withinWord()
+		{
+		//Arrange
+		$test_FindReplace = new FindReplace;
+		$replaceThis = 'world';
+		$replaceWithThis = 'mars';
+		$sentenceToBeChanged = 'Hello worldwide what such a beautiful world';
+
+		//Act
+		$result = $test_FindReplace->findReplaceFunction($replaceThis, $replaceWithThis, $sentenceToBeChanged);
+
+		//Assert
+		$this->assertEquals('Hello marswide what such a beautiful mars', $result);
+		}
 	}
 
 ?>
